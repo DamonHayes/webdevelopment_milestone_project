4881369214b73004ea809ddacc0870abf28f90c8
DamonHayes <92762805+DamonHayes@users.noreply.github.com>
2022-02-09 16:29:23 -0600

    moved index.js into the js folder

--- js/index.js
+++ js/index.js
@@ -42,6 +42,7 @@ class Player {
 	}
 }
 
+// the platform class this creates a singular platform and colors it blue
 class Platform {
 	constructor () {
 		this.position = {
@@ -58,9 +59,12 @@ class Platform {
 	}
 }
 
+//creating varibles for the player and platform class
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform()]
 
+
+// const vars for the state of the right and left keys
 const keys = {
 	right: {
 		pressed: false
@@ -70,12 +74,15 @@ const keys = {
 	}
 }
 
-
+// the function that handles the collision and player movement this 
 function animate() {
 	requestAnimationFrame(animate)
 	context.clearRect(0, 0, canvas.width, canvas.height)
 	player.update()
-	platform.draw()
+	platforms.forEach(platform=> {
+		platform.draw()
+	})
+	
 	
 	if (keys.right.pressed && player.position.x < 401) {
 		player.velocity.x = 5
@@ -85,20 +92,26 @@ function animate() {
 		player.velocity.x = 0
 		
 		if (keys.right.pressed) {
-			platform.position.x -= 5
+			platforms.forEach(platform=> {
+				platform.position.x -= 5
+			})
 		} else if (keys.left.pressed) {
-			platform.position.x += 5
+			platforms.forEach(platform=> {
+				platform.position.x += 5
+			})
 		}
 	}
-	
-	if (player.position.y  + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
-		player.velocity.y = 0
-	}
+	platforms.forEach(platform=> {
+		if (player.position.y  + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+			player.velocity.y = 0
+		}
+	})
 }
 
-
-
 animate()
+
+
+//key listeners for the WASD keys for player movement
 
 window.addEventListener('keydown', ({keyCode}) => {
 	switch (keyCode) {
